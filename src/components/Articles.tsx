33d8c7523161d2630c4bddc6b5b31671a1b188e5
y.miyake <yuto.miyake@nijibox.co.jp>
2023-09-06 16:24:00 +0900

    title部分を実装

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -2,6 +2,7 @@ import { ZENN_POST_TYPE } from "@/types/zennTypes";
 import { FC, memo } from "react";
 
 import styles from "@/styles/components/articles.module.scss";
+import Link from "next/link";
 
 interface Props {
   zennPosts: ZENN_POST_TYPE[];
@@ -11,7 +12,25 @@ const Articles: FC<Props> = ({ zennPosts }) => {
   return (
     <div className={styles.articles}>
       <div className={styles.articles__content}>
-        <article>hoge</article>
+        {zennPosts.map((post) => (
+          <article className={styles.articles__article} key={post.id}>
+            <Link
+              className={styles.articles__articleLinkWrap}
+              href={post.url}
+              target="_blank"
+            >
+              <div className={styles.articles__emojiWrap}>
+                <span>{post.emoji}</span>
+                <div className={styles.articles__articleType}>
+                  {post.articleType}
+                </div>
+              </div>
+              <div className={styles.articles__titleWrap}>
+                <h3 className={styles.articles__title}>{post.title}</h3>
+              </div>
+            </Link>
+          </article>
+        ))}
       </div>
     </div>
   );

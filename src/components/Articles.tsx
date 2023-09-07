1583c564ceacd1c81f9782b3e7b1dbe63eff5fd0
y.miyake <yuto.miyake@nijibox.co.jp>
2023-09-07 10:33:23 +0900

    レシぽん支部対応

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -12,25 +12,27 @@ const Articles: FC<Props> = ({ zennPosts }) => {
   return (
     <div className={styles.articles}>
       <div className={styles.articles__content}>
-        {zennPosts.map((post) => (
-          <article className={styles.articles__article} key={post.id}>
-            <Link
-              className={styles.articles__articleLinkWrap}
-              href={post.url}
-              target="_blank"
-            >
-              <div className={styles.articles__emojiWrap}>
-                <span>{post.emoji}</span>
-                <div className={styles.articles__articleType}>
-                  {post.articleType}
+        <div className={styles.articles__articleWrap}>
+          {zennPosts.map((post) => (
+            <article className={styles.articles__article} key={post.id}>
+              <Link
+                className={styles.articles__articleLinkWrap}
+                href={post.url}
+                target="_blank"
+              >
+                <div className={styles.articles__emojiWrap}>
+                  <span>{post.emoji}</span>
+                  <div className={styles.articles__articleType}>
+                    {post.articleType}
+                  </div>
                 </div>
-              </div>
-              <div className={styles.articles__titleWrap}>
-                <h3 className={styles.articles__title}>{post.title}</h3>
-              </div>
-            </Link>
-          </article>
-        ))}
+                <div className={styles.articles__titleWrap}>
+                  <h3 className={styles.articles__title}>{post.title}</h3>
+                </div>
+              </Link>
+            </article>
+          ))}
+        </div>
       </div>
     </div>
   );
